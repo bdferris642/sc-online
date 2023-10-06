261c8dc418f245b434ea113513bc0a301fecc1f2
Bennett Ferris <bennett.ferris@gmail.com>
2023-10-06 11:50:58 -0400

    VG original code for extraGenes amd Exp Set Creator functions

---
+++ extraGenes.R
@@ -0,0 +1,162 @@
+.extraMitoGenes=function(organism,redownload_files=T){
+  #organism: Human, Mouse
+  
+  if(tolower(organism)=="human"){
+    gns=.extraHumanGeneAnnoAdderFn()
+  } else if (tolower(organism)=="mouse"){
+    gns=.extraMouseGeneAnnoAdderFn()
+  } else if(tolower(organism)=="macaque"){
+    gns=.extraMacaqueGeneAnnoAdderFn()
+  } else {
+    stop("Wrong organism name!")
+  }
+  
+  gns=gns[gns$seqnames=="MT",]
+  gns2=unlist(gns$entrezid)
+  gns$entrezgene_id=gns2
+  gns$ensembl_gene_id=gns$gene_id
+  return(gns)
+}
+
+.extrarRNAGenes=function(organism,redownload_files=T){
+  #organism: Human, Mouse
+  
+  if(tolower(organism)=="human"){
+    gns=.extraHumanGeneAnnoAdderFn()
+  } else if (tolower(organism)=="mouse"){
+    gns=.extraMouseGeneAnnoAdderFn()
+  } else if(tolower(organism)=="macaque"){
+    gns=.extraMacaqueGeneAnnoAdderFn()
+  } else {
+    stop("Wrong organism name!")
+  }
+  
+  gns=gns[which(gns$gene_biotype=="rRNA"),]
+  gns2=unlist(gns$entrezid)
+  gns$entrezgene_id=gns2
+  gns$ensembl_gene_id=gns$gene_id
+  return(gns)
+}
+
+.extraRibosomalProteinGenes=function(organism,redownload_files=T){
+  #organism: Human, Mouse
+  
+  if(tolower(organism)=="human"){
+    gns=.extraHumanGeneAnnoAdderFn()
+  } else if (tolower(organism)=="mouse"){
+    gns=.extraMouseGeneAnnoAdderFn()
+  } else if(tolower(organism)=="macaque"){
+    gns=.extraMacaqueGeneAnnoAdderFn()
+  } else {
+    stop("Wrong organism name!")
+  }
+  
+  gns=gns[which(grepl("^RP[LS]",gns$gene_short_name,ignore.case = T)),]
+  gns2=unlist(lapply(gns$entrezid,function(x) x[1]))
+  gns$entrezgene_id=gns2
+  gns$ensembl_gene_id=gns$gene_id
+  return(gns)
+}
+
+
+.extraOXPHOSGenes=function(organism,inputGeneset=NULL,server=F){
+  #organism: Human, Mouse
+  
+  if(tolower(organism)=="human"){
+    gns=.extraHumanGeneAnnoAdderFn()
+  } else if (tolower(organism)=="mouse"){
+    gns=.extraMouseGeneAnnoAdderFn()
+  } else if(tolower(organism)=="macaque"){
+    gns=.extraMacaqueGeneAnnoAdderFn()
+  } else {
+    stop("Wrong organism name!")
+  }
+  
+  if(is.null(inputGeneset)){
+      if(!file.exists("~/serverFiles/OXPHOS_gene_symbols.txt")){
+        stop("Error! file ~/serverFiles/OXPHOS_gene_symbols.txt is missing")
+      }
+      
+      oxphosGenes=read.table("~/serverFiles/OXPHOS_gene_symbols.txt",sep="\t",stringsAsFactors = F)
+    
+  }
+  
+  if(sum(colnames(gns)=="gene_short_name")>0){
+    gns=gns[which(toupper(gns$gene_short_name) %in% toupper(oxphosGenes[,1])),]
+  } else {
+    gns=gns[which(toupper(gns$symbol) %in% toupper(oxphosGenes[,1])),]
+  }
+  
+  
+  return(gns)
+}
+
+.extraIEGGenes=function(organism,server=F){
+  #organism: Human, Mouse
+  
+  gns=.extraMouseGeneAnnoAdderFn()
+  
+  {
+    #library(googleCloudStorageR)
+    stop("Error! file serverFiles/IEG_gene_symbols is missing")
+    
+    IEGenes=read.table("~/serverFiles/IEG_gene_symbols",sep="\t",stringsAsFactors = F)
+  }
+  
+  gns=gns[tolower(gns$gene_name) %in% tolower(IEGenes$V1),]
+  
+  if(tolower(organism)=="human"){
+    
+    {
+      #library(googleCloudStorageR)
+      if(!file.exists("~/serverFiles/ortholog_mapping.rda")){
+        stop("Error! file ~/serverFiles/ortholog_mapping.rda is missing")
+        #gcs_get_object("vgazesta/serverFiles/orthologsFeb3/ortholog_mapping.rda", saveToDisk = "~/serverFiles/ortholog_mapping.rda",overwrite=T)
+      }
+      
+      load("~/serverFiles/ortholog_mapping.rda")
+    }
+    
+    
+    
+    mapping=mapping[mapping$mouse %in% gns$ensembl_gene_id,]
+    gnsHuman=.extraHumanGeneAnnoAdderFn()
+    gns=gnsHuman[gnsHuman$ensembl_gene_id %in% mapping$human,]
+  } else if (tolower(organism)=="mouse"){
+    
+  } else if(tolower(organism)=="macaque"){
+    gns=.extraMacaqueGeneAnnoAdderFn()
+    gns=gns[which(tolower(gns$symbol) %in% tolower(IEGenes$V1)|tolower(gns$gene_name) %in% tolower(IEGenes$V1)),]
+  } else {
+    stop("Wrong organism name!")
+  }
+  
+  gns$ensembl_gene_id=gns$gene_id
+  return(gns)
+}
+
+.extraCellCycleGenes=function(organism){
+  if(tolower(organism)=="human"){
+    gns=.extraHumanGeneAnnoAdderFn()
+  } else if (tolower(organism)=="mouse"){
+    gns=.extraMouseGeneAnnoAdderFn()
+  } else if(tolower(organism)=="macaque"){
+    gns=.extraMacaqueGeneAnnoAdderFn()
+  } else {
+    stop("Wrong organism name!")
+  }
+  
+  cc.s=Seurat::cc.genes.updated.2019$s.genes
+  cc.g2m=Seurat::cc.genes.updated.2019$g2m.genes
+  
+  cc.g2m[cc.g2m=="PIMREG"]="FAM64A"
+  cc.g2m[cc.g2m=="JPT1"]="HN1"
+  
+  cc.s=gns[toupper(gns$symbol) %in% toupper(cc.s),]
+  cc.g2m=gns[toupper(gns$symbol) %in% toupper(cc.g2m),]
+  
+  cc.s$ensemble_gene_id=row.names(cc.s)
+  cc.g2m$ensemble_gene_id=row.names(cc.g2m)
+  
+  return(list(cc.s=cc.s,cc.g2m=cc.g2m))
+}
---
+++ myExpSetCreatorFn.R
@@ -0,0 +1,152 @@
+.myExpSetCreatorFn=function(inputExpData,organism,minExpCells=0,inputPdata=NULL,inputFdata=NULL,addExtraAnno=T,server=T,redownload_files=T,ncores=5){
+  require(scran)
+  require(DropletUtils)
+  require(Matrix)
+  if(is.null(colnames(inputExpData))){
+    colnames(inputExpData)=1:ncol(inputExpData)
+  }
+  
+  if(tolower(class(inputExpData))=="seurat"){
+    inputExpData=.sconline.convertSeuratToExpressionSet(object=inputExpData)
+  }
+  
+  if(is(inputExpData,"SingleCellExperiment")){
+    tmp=as.data.frame(rowData(inputExpData))
+    if(is.null(inputFdata)){
+      inputFdata=tmp
+    } else {
+      inputFdata=cbind(inputFdata,tmp)
+    }
+    
+    tmpPdata=as.data.frame(colData(inputExpData))
+    if(is.null(inputPdata)){
+      inputPdata=tmpPdata
+    } else {
+      inputPdata=cbind(inputPdata,tmpPdata)
+    }
+  }
+  
+  if(is(inputExpData,"Seurat")){
+    tmpPdata=as.data.frame(inputPdata[[]])
+    if(is.null(inputPdata)){
+      inputPdata=tmpPdata
+    } else {
+      inputPdata=cbind(inputPdata,tmpPdata)
+    }
+  }
+  
+  
+  if(!is.null(inputPdata)){
+    pd <- inputPdata
+  } else {
+    pd=data.frame(sample=colnames(inputExpData),stringsAsFactors = F)
+    row.names(pd)=colnames(inputExpData)
+  }
+  
+  if(tolower(organism)=="human"){
+    tmpName=.extraHumanGeneAnnoAdderFn(row.names(inputExpData),server = server)
+  } else if (tolower(organism)=="mouse"){
+    tmpName=.extraMouseGeneAnnoAdderFn(row.names(inputExpData),server = server)
+  } else if(tolower(organism)=="macaque"){
+    tmpName=.extraMacaqueGeneAnnoAdderFn(row.names(inputExpData),server = server)
+  } else {
+    warning("unknown organism!")
+  }
+  
+  fd=NULL
+  if(tolower(organism) %in% c("human","mouse","macaque")){
+    if(all(row.names(inputExpData)==row.names(tmpName))){
+    fd=tmpName
+    
+    if(!is.null(inputFdata)){
+      fd=cbind(inputFdata,fd)
+    }
+    if(sum(colnames(fd)=="ID")==0){
+      fd$ID=row.names(inputExpData)
+    }
+    row.names(fd)=row.names(inputExpData)
+    
+    if(tolower(organism) %in% c("mouse","human","macaque")){
+      mycc.genes=.extraCellCycleGenes(organism = organism)
+      fd$QC_ssGenes="No"
+      fd$QC_g2mGenes="No"
+      fd$QC_ssGenes[fd$ensembl_gene_id %in% row.names(mycc.genes$cc.s)]="s_phase"
+      fd$QC_g2mGenes[fd$ensembl_gene_id %in% row.names(mycc.genes$cc.g2m)]="g2m_phase"
+      
+      mymito.genes=.extraMitoGenes(organism=organism)
+      fd$QC_mtGenes="No"
+      fd$QC_mtGenes[fd$ensembl_gene_id %in% mymito.genes$ensembl_gene_id]="Yes"
+      
+      myIEG.genes=.extraIEGGenes(organism=organism,server = server)
+      fd$QC_IEG_Genes="No"
+      fd$QC_IEG_Genes[fd$ensembl_gene_id %in% myIEG.genes$ensembl_gene_id]="Yes"
+    }
+   
+    
+  } else {
+      print("Error!")
+    }
+    if(sum(colnames(fd) %in% c("seqnames", "ranges", "strand", "start", "end", "width","element"))>0){
+      colnames(fd)[colnames(fd) %in% c("seqnames", "ranges", "strand", "start", "end", "width","element")]=paste0("anno_",colnames(fd)[colnames(fd) %in% c("seqnames", "ranges", "strand", "start", "end", "width","element")])
+      
+    }
+  } else {
+    fd=inputFdata
+  }
+  
+  
+  
+  if(class(inputExpData)=="SingleCellExperiment"){
+    res=counts(inputExpData)
+  } else {
+    if(class(inputExpData)!="dgCMatrix"){
+      if(class(inputExpData)=="data.frame"){
+        res=Seurat::as.sparse(inputExpData)
+      } else {
+        res=.matrixExtraction(inputExpData)
+      }
+      row.names(res)=row.names(inputExpData)
+      colnames(res)=colnames(inputExpData)
+    } else {
+      res=inputExpData
+    }
+    
+  }
+  
+  colnames(res)=colnames(inputExpData)
+  
+  res <- as(res, "dgCMatrix")
+  
+  res=SingleCellExperiment(assays = list(counts = res),colData = pd,rowData=fd)
+  rm(inputExpData)
+  
+  if(minExpCells>0){
+    tmpFilter=c()
+    
+    if(nrow(res)>10000){
+      for(i in seq(1,nrow(res),10000)){
+        tmpFilter=c(tmpFilter,apply(assays(res)[["counts"]][i:min(i+10000-1,nrow(res)),],1,function(x) sum(x>0)))
+      }
+    } else {
+      tmpFilter=apply(assays(res)[["counts"]],1,function(x) sum(x>0))
+    }
+    
+    
+    res=res[which(tmpFilter>=minExpCells),]
+  }
+  
+  
+  
+  if(addExtraAnno){
+    #inputExpSet = res;organism=organism;server=server;redownload_files=redownload_files
+    res=.extraQCAnnoAdderFn(inputExpSet = res,organism=organism,server=server,redownload_files=redownload_files,ncores=ncores)
+  }
+  
+  for(i in 1:ncol(colData(res))){
+    if(class(colData(res)[,i])=="factor"){
+      colData(res)[,i]=droplevels(colData(res)[,i])
+    }
+  }
+  
+  return(res)
+}
