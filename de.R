eee58f816be5f64281eba597e579842e7234fceb
Bennett Ferris <bennett.ferris@gmail.com>
2025-03-31 19:28:21 +0000

    add pseudobulk_seurat() fnctn

--- de.R
+++ de.R
@@ -581,3 +581,60 @@ get_de_correspondence_mats = function(de_dfs, union_min=50, mode="union"){
     "n" = n_mat
   ))
 }
+
+pseudobulk_seurat = function(sobj, grouping_cols, assay="RNA", min_n_cells = 10, min_counts_gene = 10, min_frac_gene = 0.01, contrast_col="case_control"){
+
+    create_df_with_contrast_col = function() {
+      setNames(data.frame(factor(levels = sort(unique(md[[contrast_col]])))), contrast_col)
+    }
+    # sobj: Seurat object containing metadata columns nUMI, pct_intronic, pct_mito
+    
+    df = sobj@meta.data
+    if (! all(grouping_cols %in% colnames(df))){
+        stop(paste0("Not all grouping columns are present in the Seurat object!\n",
+            "Missing Columns: ", paste(grouping_cols[!grouping_cols %in% colnames(df)], collapse=", ")))
+    }
+    df$grouping = apply(df[grouping_cols], 1, function(row) paste(row, collapse = "_"))    
+    sobj$grouping = df$grouping
+    grouping_cols = c(grouping_cols, "grouping")
+
+    # group by participant_id
+    # sum nUMI 
+    # weighted average of pct_intronic and pct_mito (weighted by nUMI)
+    # take log10 of summed nUMI
+    # the following columns should be identical for all cells in the same participant:
+    # age, sex, brain_bank, case_control
+    cat(paste("Grouping By Columns: ", paste(grouping_cols, collapse=", "), "\n"))
+
+    df_bulk = (df 
+        %>% group_by(across(all_of(grouping_cols)))
+        %>% summarise(
+            n_cells = n(),
+            sum_nUMI = sum(nUMI),
+            pct_mito = weighted.mean(pct_mito, nUMI),
+            pct_intronic = weighted.mean(pct_intronic, nUMI))
+        %>% mutate(log10_nUMI = log10(sum_nUMI))
+    )
+    df_bulk = df_bulk[df_bulk$n_cells >= min_n_cells,]
+    if (nrow(df_bulk) == 0){
+        return(list(counts=NULL,  metadata=create_df_with_contrast_col()))
+    }
+
+    sobj = sobj[,df$grouping %in% df_bulk$grouping]
+    
+    counts_bulk = AggregateExpression(sobj, group.by = "grouping")[[assay]]
+    
+    counts_orig = sobj@assays[[assay]]@counts
+    counts_bulk = counts_bulk[rowSums(counts_orig)>= min_counts_gene & rowMeans(counts_orig > 0) >= min_frac_gene,]
+    
+    colnames(counts_bulk) = gsub("-", '_', colnames(counts_bulk))
+    df_bulk = df_bulk[match(colnames(counts_bulk), df_bulk$grouping),]
+    
+    if (nrow(df_bulk) == 0){
+        return(list(counts=NULL, metadata=create_df_with_contrast_col()))
+    }
+
+    return(
+        list(counts=counts_bulk, metadata=df_bulk)
+    )
+}
