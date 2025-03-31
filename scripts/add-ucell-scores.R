771631d7aeaa8dcfe952b2fc9c9fc44af8447657
Bennett Ferris <bennett.ferris@gmail.com>
2025-03-31 19:21:27 +0000

    more scripts

---
+++ scripts/add-ucell-scores.R
@@ -0,0 +1,65 @@
+# path to a tsv file with columns "score_name" and "genes". 
+# The genes column should be in the format of a comma-delimited string.
+
+print("Loading Libraries")
+suppressWarnings(suppressMessages(library(getopt)))
+suppressWarnings(suppressMessages(library(Seurat)))
+suppressWarnings(suppressMessages(library(qs)))
+suppressWarnings(suppressMessages(library(dplyr)))
+suppressWarnings(suppressMessages(library(UCell)))
+
+spec <- matrix(c(
+    'sobj-path', 'p', 1, "character",
+    'score-path', 's', 1, "character",
+    'assay', 'a', 1, "character"
+), byrow = TRUE, ncol = 4)
+opt = getopt(spec)
+
+SOBJ_PATH = opt[["sobj-path"]]
+SCORE_PATH = opt[["score-path"]]
+if(is.null(opt[['assay']])) {
+    ASSAY = "RNA"
+} else {
+    ASSAY = opt[["assay"]]
+}
+
+print( paste0("Reading gene scores from ", SCORE_PATH))
+score_list_df = read.table(SCORE_PATH, header=TRUE, sep="\t", stringsAsFactors=FALSE)
+print(head(score_list_df))
+gene_sets = lapply(score_list_df$genes, function(genes) {
+    strsplit(genes, ",")[[1]]
+})
+names(gene_sets) = score_list_df$score_name
+
+print( paste("Reading Seurat object from", SOBJ_PATH))
+sobj = qread(SOBJ_PATH)
+
+for (name in names(gene_sets)){
+    gene_sets[[name]] = gene_sets[[name]][gene_sets[[name]] %in% rownames(sobj@assays[[ASSAY]]$counts)]
+    print(paste0("Calculating score for ", name))
+    print("Genes:")
+    print(paste(gene_sets[[name]][1:5], collapse=', '))
+    print("...")
+}
+print(sapply(gene_sets, length))
+
+missing_genes <- setdiff(unlist(gene_sets), rownames(sobj@assays[[ASSAY]]$counts))
+if (length(missing_genes) > 0) {
+    print(paste("Missing genes:", paste(missing_genes, collapse = ", ")))
+}
+
+print("Calculating U Scores")
+u_scores = ScoreSignatures_UCell(
+    as.matrix(sobj@assays[[ASSAY]]$counts), gene_sets, assay = ASSAY)
+
+u_scores = u_scores[match(rownames(sobj@meta.data), rownames(u_scores)),]
+u_scores = as.data.frame(u_scores)
+
+if (all(rownames(u_scores) == rownames(sobj@meta.data))){
+    for (score_name in colnames(u_scores)){
+        sobj@meta.data[[paste0(score_name, "__", ASSAY)]] = u_scores[[score_name]]
+    }
+}
+
+print("Saving Seurat object")
+qsave(sobj, SOBJ_PATH)
---
+++ scripts/convert-seurat-to-anndata-h5ad.R
@@ -0,0 +1,52 @@
+library(Seurat)
+library(SeuratDisk)
+library(anndata)
+library(getopt)
+library(Matrix)
+library(qs)
+
+
+
+# Parse command line arguments
+spec = matrix(c(
+    'path', 'p', 1, "character"
+), byrow = TRUE, ncol = 4)
+opt = getopt(spec)
+PATH = opt[['path']]
+
+
+dirname = dirname(PATH)
+basename = basename(PATH)
+slogan = gsub(".qs", "", basename)
+h5seurat_fname = paste0(dirname, "/", slogan, ".h5Seurat")
+
+library(Seurat)
+library(SeuratDisk)
+
+# Load Seurat object
+seurat_obj <- qread(PATH)
+DefaultAssay(seurat_obj) <- "RNA"
+
+# Keep only the RNA assay
+# if seurat object version is 5 or later, counts is accessed with $, 4 or earlier with @
+
+if (seurat_obj@version >= 5) {
+    seurat_obj[["RNA"]] <- CreateAssayObject(counts = seurat_obj@assays$RNA$counts)
+} else {
+    seurat_obj[["RNA"]] <- CreateAssayObject(counts = seurat_obj@assays$RNA@counts)
+}
+
+
+# Remove all other assays
+seurat_obj@assays <- list(RNA = seurat_obj[["RNA"]])
+
+# Ensure metadata is properly aligned
+if (!all(rownames(seurat_obj@meta.data) == colnames(seurat_obj))) {
+    stop("Cell names in meta.data do not match counts matrix! Check alignment.")
+}
+
+# Save the cleaned Seurat object
+SaveH5Seurat(seurat_obj, filename = h5seurat_fname, overwrite = TRUE)
+
+# Convert to H5AD
+Convert(h5seurat_fname, dest = "h5ad", overwrite = TRUE)
