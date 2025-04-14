dd98a644330e52546affce62208b29729d60e234
Bennett Ferris <bennett.ferris@gmail.com>
2025-04-14 13:49:58 +0000

    limma code that is runs without contrast col

---
+++ scripts/run-limma-v2.R
@@ -0,0 +1,279 @@
+########################################## IMPORTS ###########################################
+
+print("Loading libraries...")
+
+suppressWarnings(suppressMessages(source("~/code/sconline_code.R")))
+suppressWarnings(suppressMessages(library(getopt)))
+suppressWarnings(suppressMessages(library(Matrix)))
+suppressWarnings(suppressMessages(library(Seurat)))
+suppressWarnings(suppressMessages(library(SingleCellExperiment)))
+suppressWarnings(suppressMessages(library(tidyr)))
+
+suppressWarnings(suppressMessages(source("/home/ferris/code/sconline_code.R"))) # <-- TODO inherit from de.R
+suppressWarnings(suppressMessages(source("/home/ferris/sc-online/utils.R")))
+########################################### ARGUMENTS & CONSTANTS ###########################################
+
+spec <- matrix(c(
+    'path', 'p', 1, "character",
+    'bkg-gene-pct', 'bgp', 1, "numeric",
+    'bkg-gene-count', 'bgc', 1, "numeric",
+    'contrast-col', 'cnc', 1, "character",
+    'cluster-col', 'clc', 1, "character",
+    'cov-list', 'cl', 1, "character",
+    'rand-var', 'rv', 1, "character",
+    'de-method', 'dem', 1, "character",
+    'only-clusters', 'o', 1, 'character',
+    'suffix', 's', 1, 'character'
+), byrow = TRUE, ncol = 4)
+opt <- getopt(spec)
+
+print(spec)
+print(opt)
+
+BKG_GENE_COUNT_THR = ifelse(
+    is.null(opt[['bkg-gene-count']]), 
+    10, 
+    opt[['bkg-gene-count']])
+print(paste('bkg-gene-count:', BKG_GENE_COUNT_THR))
+
+BKG_GENE_PCT_THR = ifelse(
+    is.null(opt[['bkg-gene-pct']]), 
+    0.01, 
+    opt[['bkg-gene-pct']])
+print(paste('bkg-gene-pct:', BKG_GENE_PCT_THR))
+
+RAND_VAR = ifelse(
+    is.null(opt[['rand-var']]), 
+    "participant_id", 
+    opt[['rand-var']])
+print(paste('rand-var:', RAND_VAR))
+if (tolower(RAND_VAR) %in% c("null", "na", "none")){
+    RAND_VAR = NULL
+}
+
+if( is.null(opt[['contrast-col']])) {
+    CONTRAST_COL = NULL
+} else {
+    CONTRAST_COL = opt[['contrast-col']]
+}
+print(paste('contrast-col:', CONTRAST_COL))
+
+CLUSTER_COL = ifelse(
+    is.null(opt[['cluster-col']]), 
+    "cell_class", 
+    opt[['cluster-col']])
+print(paste('cluster-col:', CLUSTER_COL))
+
+if (is.null(opt[['cov-list']])) {
+    COV_LIST = c('case_control', 'sex', 'age', 'QC_Gene_total_log', 'pct_mito', "pct_intronic")
+} else {
+    COV_LIST = strsplit(opt[['cov-list']], ",")[[1]]
+}
+
+DE_METHOD = ifelse(
+    is.null(opt[['de-method']]),
+    "Trend",
+    opt[['de-method']])
+    
+if (is.null(opt[['only-clusters']])) {
+    ONLY_CLUSTERS = NULL
+} else {
+    ONLY_CLUSTERS = strsplit(opt[['only-clusters']], ",")[[1]]
+}
+
+if (is.null(opt[['suffix']])){
+    SUFFIX = ""
+} else {
+    SUFFIX = opt[['suffix']]
+}
+SUFFIX = paste0('__', SUFFIX, format(Sys.Date(), "__%Y%m%d"))
+
+
+PATH = opt[['path']]
+BASE_PATH = dirname(PATH)
+PSEUDOCELL_BASENAME = basename(PATH)
+
+# to get the seurat basename, take the pseudocell basename until the _pseudocell
+# and remove everything that comes after it as well. Finally, append .qs
+SEURAT_BASENAME = gsub("_pseudocell.*", ".qs", PSEUDOCELL_BASENAME)
+
+
+# hard code these for now
+QUANTILE_NORM=T
+MIN_NUM_PSEUDOCELLS=length(COV_LIST)+2
+
+
+########################################### MAIN ###########################################
+print("Reading pseudocells and seurat object data...")
+pseudocell_read_path = file.path(PATH)
+pseudocells_list = qread(pseudocell_read_path)
+
+# downstream code expects a list of pseudocell sce; 
+# if the object is just sce, make it a list
+if (class(pseudocells_list) == "SingleCellExperiment"){
+    pseudocells_list = list("all" = pseudocells_list)
+}
+
+
+de_dir = file.path(BASE_PATH, "de", gsub(".qs", "", PSEUDOCELL_BASENAME))
+if (!dir.exists(de_dir)) {
+    print(paste("Creating directory", de_dir))
+    dir.create(de_dir, recursive=TRUE)
+}
+
+# Dynamically build the contrast string based on the input parameters
+# we want ctr, ctrl, cntrl, or control to be the second category
+# note that if both categories are in the above list, whichever comes first will be category 2
+
+data_sce = pseudocells_list[[1]]
+if (! is.null(CONTRAST_COL)){
+    categories = sort(unique(colData(data_sce)[[CONTRAST_COL]]))
+    category1 = categories[[1]]
+    category2 = categories[[2]]
+    for (c in categories) {
+        if (tolower(c) %in% c("ctr", "ctrl", "cntrl", "control", "GTEx")) {
+            category2 = c
+            category1 = categories[categories != c][[1]]
+            break
+        }
+    }
+    contrast_str = sprintf("%s%s - %s%s", CONTRAST_COL, category1, CONTRAST_COL, category2)
+    print(paste("Contrast string:", contrast_str))
+}
+
+print(paste("Performing DE analysis on clusters:", paste(names(pseudocells_list), collapse=", "), "from cluster column", CLUSTER_COL, "..."))
+for (x_name in names(pseudocells_list)){
+
+    if (! is.null(ONLY_CLUSTERS)) {
+        print(paste("checking if", x_name, "in", ONLY_CLUSTERS))
+        if (! x_name %in% ONLY_CLUSTERS) {
+            print(paste("Skipping", x_name))
+            next
+        }
+    }
+
+    print(x_name)
+    x=pseudocells_list[[x_name]]
+
+    if (! is.null(CONTRAST_COL)){
+        if (! all(c(CONTRAST_COL, COV_LIST, RAND_VAR, CLUSTER_COL) %in% colnames(colData(x)))){
+            print("Missing columns in pseudocell data:")
+            print(c(CONTRAST_COL, COV_LIST, RAND_VAR, CLUSTER_COL)[! c(CONTRAST_COL, COV_LIST, RAND_VAR, CLUSTER_COL) %in% colnames(colData(x))])
+            stop("Missing columns in pseudocell data! Exiting!")
+            next
+        }
+        cd = colData(x)[, c(CONTRAST_COL, COV_LIST, RAND_VAR, CLUSTER_COL)]
+        # todo: fix this, parametrized
+        x = x[, complete.cases(cd) & cd[[CONTRAST_COL]] != ""]
+    } else {
+        if (! all(c(COV_LIST, RAND_VAR, CLUSTER_COL) %in% colnames(colData(x)))){
+            print("Missing columns in pseudocell data:")
+            print(c(COV_LIST, RAND_VAR, CLUSTER_COL)[! c(COV_LIST, RAND_VAR, CLUSTER_COL) %in% colnames(colData(x))])
+            stop("Missing columns in pseudocell data! Exiting!")
+            next
+        }
+        cd = colData(x)[, c(COV_LIST, RAND_VAR, CLUSTER_COL)]
+        x = x[, complete.cases(cd)]
+    }
+
+    print(paste0("num pseudocells: ", ncol(x)))
+
+    res=NULL
+    res_pd=NULL
+
+    if(ncol(x)>=MIN_NUM_PSEUDOCELLS){
+        print(paste0("Calculating DE genes for ", x_name))
+        print(dim(x))
+        
+        if(!is.null(BKG_GENE_COUNT_THR)){
+
+            anno = as.data.frame(colData(data_sce))[[CLUSTER_COL]]
+            
+            tmp_bkg_genes=counts(data_sce)[,which(anno==x_name)]
+            
+            tmp_bkg_genes_counts=rowSums(tmp_bkg_genes>0)
+
+            tmp_bkg_genes_frac=tmp_bkg_genes_counts/sum(anno==x_name)
+
+            tmp_bkg_genes=row.names(data_sce)[tmp_bkg_genes_frac>=BKG_GENE_PCT_THR&tmp_bkg_genes_counts>=BKG_GENE_COUNT_THR]
+        } else {
+            tmp_bkg_genes=NULL
+        }
+
+        print(paste("Number of Background genes", length(tmp_bkg_genes)))
+        tryCatch({
+            res=.sconline.fitLimmaFn(
+                inputExpData=x,
+                DEmethod=DE_METHOD,
+                covariates=COV_LIST,
+                randomEffect=RAND_VAR,
+                bkg_genes=tmp_bkg_genes,
+                quantile.norm=QUANTILE_NORM,
+                prior.count=1) #TODO: check if this is the right value for prior.count
+            #check the dc object, the usual consensus.correlation that I get is in the range of ~0.2 or above if rand=T
+            
+            write_path = file.path(de_dir, paste0(tolower(DE_METHOD), '__', x_name, SUFFIX, '.qs'))
+
+            if (! is.null(CONTRAST_COL)){
+                contr = makeContrasts(contrasts=contrast_str, levels=res$model)
+
+                fit2=contrasts.fit(res$fit,contrasts=contr)
+            
+                # TODO: Explore setting robust=F
+                fit2=eBayes(fit2, robust=T, trend=T)
+
+                res_pd = topTable(
+                    fit2,
+                    number=dim(fit2)[1],
+                    adjust.method="BH",
+                    coef=contrast_str)
+
+                res_pd = res_pd[order(-res_pd$logFC),]
+                res_pd$gene = rownames(res_pd)
+
+                
+                csv_write_path = gsub(".qs", paste0("__", CONTRAST_COL, ".csv"), write_path)
+                write.csv(res_pd, csv_write_path, row.names=FALSE)
+            } else { 
+                res_pd = "NONE"
+            }
+
+            # also make top_tables for other coefs
+            fit2=eBayes(res$fit, robust=T, trend=T)
+
+            output_list = list(
+                'res' = res,
+                'res_pd' = res_pd,
+                'pseudocells' = pseudocell_read_path, 
+                'cluster_col' = CLUSTER_COL,
+                'cluster' = x_name,
+                'de_method' = DE_METHOD,
+                'contrast_col' = CONTRAST_COL,
+                'cov_list' = COV_LIST,
+                'rand_var' = RAND_VAR,
+                'quantile_norm' = QUANTILE_NORM,
+                'min_num_pseudocells' = MIN_NUM_PSEUDOCELLS
+            )
+
+            for(coef in colnames(res$fit$coefficients)){
+                tt = topTable(
+                    fit2,
+                    number=dim(fit2)[1],
+                    adjust.method="BH",
+                    coef=coef)
+
+                tt = tt[order(-tt$logFC),]
+                tt$gene = rownames(tt)
+                write.csv(tt, gsub(".qs", paste0("__", coef, ".csv"), write_path))
+                output_list[[paste0('tt_', coef)]] = tt 
+            }
+
+            qsave(output_list, write_path)
+            
+
+        }, error=function(e){
+            print(paste("Error in DE analysis for", x_name))
+            print(e)
+        })
+    }
+}
